574eb7ff427630b7af72ac7fa34e9e0988cb96cb
enterfaceteam <guijet2@gmail.com>
2017-09-05 22:04:37 -0400

    Test

--- GeekCafe-iOS/SignUp5.swift
+++ GeekCafe-iOS/SignUp5.swift
@@ -12,7 +12,7 @@ class SignUp5: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("TESST GITHUB")
         // Do any additional setup after loading the view.
     }
 
