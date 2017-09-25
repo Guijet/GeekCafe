fcb9220b7edfb13e1122e972c050b0d5ffad7678
enterfaceteam <guijet2@gmail.com>
2017-09-24 20:19:33 -0400

    List brevage et liste de patisserie terminer dans le flow de commande

--- GeekCafe-iOS/CartCommmande.swift
+++ GeekCafe-iOS/CartCommmande.swift
@@ -12,7 +12,7 @@ class CartCommmande: UIViewController {
 
     let backgroundImage = UIImageView()
     let scrollView = UIScrollView()
-    var arrayHistory = [HistoryList]()
+    var arrayItem = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,8 @@ class CartCommmande: UIViewController {
         setNavigationTitle()
         backgroundImage.setUpBackgroundImage(containerView: self.view)
         setUpScrollView()
+        fillScrollView()
+        setBottomView()
     }
     
     //To make bar all white non translucent and appearing
@@ -38,78 +40,82 @@ class CartCommmande: UIViewController {
     func setUpScrollView(){
         scrollView.frame = CGRect(x: 0, y: 64, width: view.frame.width, height: ((view.frame.height - 64) - rh(75)))
         scrollView.showsVerticalScrollIndicator = false
+        scrollView.layer.masksToBounds = false
         scrollView.showsHorizontalScrollIndicator = false
-        scrollView.backgroundColor = .red
         view.addSubview(scrollView)
     }
     
     func fillScrollView(){
-        var newY:CGFloat = rh(25)
-        var index:Int = 0
-        if(arrayHistory.count > 0){
-            for x in arrayHistory{
+        var newY:CGFloat = rh(5)
+        if(arrayItem.count > 0){
+            for x in arrayItem{
                 
-                let button = UIButton()
-                button.frame = CGRect(x: 0, y: newY, width: view.frame.width, height: rh(71))
-                if(index % 2 == 0){button.backgroundColor = Utility().hexStringToUIColor(hex: "#FFFFFF")}
-                else{button.backgroundColor = Utility().hexStringToUIColor(hex: "#FBFBFB")}
-                button.tag = x.id
-                scrollView.addSubview(button)
+                let containerView = UIView()
+                containerView.frame = CGRect(x: 0, y: newY, width: view.frame.width, height: 72)
+                containerView.backgroundColor = UIColor.clear
+                scrollView.addSubview(containerView)
                 
-                let date = UILabel()
-                date.frame = CGRect(x: rw(27), y: rh(20), width: view.frame.midX - rw(27), height: rh(18))
-                date.textColor = Utility().hexStringToUIColor(hex: "#AFAFAF")
-                date.font = UIFont(name: "Lato-Regular", size: rw(15))
-                date.textAlignment = .left
-                date.text = x.date
-                button.addSubview(date)
-                
-                let location = UILabel()
-                location.frame = CGRect(x: rw(27), y: date.frame.maxY + rh(2), width: view.frame.midX - rw(27), height: rh(13))
-                location.textColor = Utility().hexStringToUIColor(hex: "#D6D6D6")
-                location.font = UIFont(name: "Lato-Regular", size: rw(13))
-                location.textAlignment = .left
-                location.text = "\(x.city), \(x.country)"
-                button.addSubview(location)
+                let imageItem = UIImageView()
+                imageItem.frame = CGRect(x: rw(15), y: rh(6), width: rw(60), height: rw(60))
+                imageItem.image = x.image
+                containerView.addSubview(imageItem)
                 
                 let price = UILabel()
-                price.frame = CGRect(x: view.frame.midX, y: rh(26), width: view.frame.midX - rw(17), height: rh(18))
+                price.frame = CGRect(x: rw(260), y: (containerView.frame.height/2) - rh(10), width: rw(100), height: rh(20))
                 price.textColor = Utility().hexStringToUIColor(hex: "#AFAFAF")
                 price.font = UIFont(name: "Lato-Regular", size: rw(18))
                 price.textAlignment = .right
-                price.text = "$ \(x.amount)"
-                button.addSubview(price)
+                price.text = "$\(x.price)"
+                containerView.addSubview(price)
                 
-                Utility().createHR(x: 0, y: button.frame.height - 1, width: view.frame.width, view: button, color: Utility().hexStringToUIColor(hex: "#979797").withAlphaComponent(0.25))
+                let flavour = UILabel()
+                flavour.frame = CGRect(x: rw(85), y: ((containerView.frame.height/2) - rh(18)), width: price.frame.minX - rh(85), height: rh(18))
+                flavour.textColor = Utility().hexStringToUIColor(hex: "#AFAFAF")
+                flavour.font = UIFont(name: "Lato-Regular", size: rw(15))
+                flavour.textAlignment = .left
+                flavour.text = "\(x.flavour)"
+                containerView.addSubview(flavour)
                 
-                newY += rh(71)
-                index += 1
-                
+                let type = UILabel()
+                type.frame = CGRect(x: rw(85), y: ((containerView.frame.height/2)), width: price.frame.minX - rh(85), height: rh(10))
+                type.textColor = Utility().hexStringToUIColor(hex: "#D6D6D6")
+                type.font = UIFont(name: "Lato-Regular", size: rw(13))
+                type.textAlignment = .left
+                type.text = "\(x.type)"
+                containerView.addSubview(type)
+                newY += (72 + rh(10))
             }
             scrollView.contentSize = CGSize(width: 1.0, height: newY)
         }
     }
     
+    func setBottomView(){
+        let bottomView = UIView()
+        bottomView.frame = CGRect(x: 0, y: rh(602), width: view.frame.width, height: rh(75))
+        bottomView.backgroundColor = UIColor.white
+        bottomView.makeShadow(x: 0, y: 2, blur: 4, cornerRadius: 0.1, shadowColor: UIColor.black, shadowOpacity: 0.40, spread: 5)
+        view.addSubview(bottomView)
+        
+        let closeButton = UIButton()
+        closeButton.createCreateButton(title: "Fermer", frame: CGRect(x: rw(21), y: rh(12), width: rw(129), height: rh(40)), fontSize: rw(20), containerView: bottomView)
+        closeButton.addTarget(self, action: #selector(closeCart), for: .touchUpInside)
+        
+        let priceLabel = UILabel()
+        priceLabel.createLabel(frame: CGRect(x: view.frame.midX, y: rh(14), width: (view.frame.width/2) - rw(30), height: rh(32)), textColor: Utility().hexStringToUIColor(hex: "#6CA642"), fontName: "Lato-Regular", fontSize: rw(26), textAignment: .right, text: "$3.50")
+        bottomView.addSubview(priceLabel)
+    }
+    
+    func closeCart(){
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
     func fillFakeInfosArray(){
-        let item1 = Item(image: UIImage(named:"item1")!, type: "Drink", flavour: "Choco", price: "3.75")
-        let item2 = Item(image: UIImage(named:"item3")!, type: "Drink", flavour: "Caramel", price: "7.50")
-        let item3 = Item(image: UIImage(named:"item1")!, type: "Drink", flavour: "Vanille", price: "4.00")
-        let item4 = Item(image: UIImage(named:"item2")!, type: "Drink", flavour: "Nature", price: "2.50")
-        let item5 = Item(image: UIImage(named:"item3")!, type: "Drink", flavour: "Nature", price: "2.50")
-        let item6 = Item(image: UIImage(named:"item1")!, type: "Drink", flavour: "Caramel", price: "2.50")
-        let item7 = Item(image: UIImage(named:"item2")!, type: "Drink", flavour: "Nature", price: "2.50")
         
-        let arrayItems:[Item] = [item1,item2,item3,item4,item4,item4,item4,item5,item6,item7]
-        
-        arrayHistory.append(HistoryList(date: "21 août 2017", country: "Canada", city: "Boisbriand", amount: "4.50", id: 1, items: arrayItems))
-        arrayHistory.append(HistoryList(date: "28 août 2017", country: "Canada", city: "Blainville", amount: "5.00", id: 2,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "04 août 2017", country: "Canada", city: "Rosemère", amount: "12.50", id: 3,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "14 août 2017", country: "Canada", city: "Laval", amount: "1.50", id: 4,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "10 août 2017", country: "Canada", city: "Montreal", amount: "7.50", id: 5,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "21 août 2017", country: "Canada", city: "Rosemère", amount: "1.75", id: 6,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "21 août 2017", country: "Canada", city: "Laval", amount: "12.75", id: 7,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "04 août 2017", country: "Canada", city: "Blainville", amount: "1.50", id: 8,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "14 août 2017", country: "Canada", city: "Boisbriand", amount: "55.30", id: 9,items: arrayItems))
-        arrayHistory.append(HistoryList(date: "14 août 2017", country: "Canada", city: "Boisbriand", amount: "55.30", id: 10,items: arrayItems))
+        arrayItem.append(Item(image: UIImage(named:"item1")!, type: "Drink", flavour: "Choco", price: "3.75"))
+        arrayItem.append(Item(image: UIImage(named:"item3")!, type: "Drink", flavour: "Caramel", price: "7.50"))
+        arrayItem.append(Item(image:UIImage(named:"item1")!, type: "Drink", flavour: "Vanille", price: "4.00"))
+        arrayItem.append(Item(image: UIImage(named:"item3")!, type: "Drink", flavour: "Choco", price: "3.75"))
+        arrayItem.append(Item(image: UIImage(named:"item2")!, type: "Drink", flavour: "Choco", price: "3.75"))
+        arrayItem.append(Item(image: UIImage(named:"item1")!, type: "Drink", flavour: "Choco", price: "3.75"))
     }
 }
